1344c801cfbb65898318b83583cb5966b5d4734d
Mateusz Smykowski <msmykowski@interia.pl>
2017-04-30 23:04:34 +0200

    API change

--- js/script.js
+++ js/script.js
@@ -1,28 +1,32 @@
 var tweetLink = 'https://twitter.com/intent/tweet?text=',
-    quoteUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&key=867576&format=jsonp&lang=en&jsonp=?';
+    quoteUrl = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1';
 
 function getQuote() {
     $.getJSON(quoteUrl, createTweet);
 }
 
 function createTweet(input) {
-    if (!input.quoteAuthor.length) {
-        input.quoteAuthor = 'Unknown author';
+    var data = input[0],
+        quoteText = $(data.content).text().trim(),
+        quoteAuthor = data.title;
+    
+    if (!quoteAuthor.length) {
+        quoteAuthor = 'Unknown author';
     }
     
-    var tweetText = 'Quote of the day - ' + input.quoteText + ' Author: ' + input.quoteAuthor;
+    var tweetText = 'Quote of the day - ' + quoteText + ' Author: ' + quoteAuthor;
     
     if (tweetText.length > 140) {
         getQuote();
     } else {
         var tweet = tweetLink + encodeURIComponent(tweetText);
-        $('.quote').text(input.quoteText);
-        $('.author').text('Author: ' + input.quoteAuthor);
+        $('.quote').text(quoteText);
+        $('.author').text('Author: ' + quoteAuthor);
         $('.tweet').attr('href', tweet);
     }
 }
 
-$(document).ready( function () {
+$(document).ready(function () {
     getQuote();
     $('.trigger').click(function () {
         getQuote();
